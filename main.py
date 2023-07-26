7adbf3c5596fb5eb1e848aa5efb7497558adced6
Fabian <ytfabiskora07@gmail.coml>
2023-07-26 23:07:28 +0200

    add pipes and collision

--- main.py
+++ main.py
@@ -10,10 +10,38 @@ def game_floor():
     screen.blit(floor_base, (floor_x_pos + 576, 750))
 
 
-def check_collision():
+def check_collision(pipes):
+    for pipe in pipes:
+        if bird_rect.colliderect(pipe):
+            return False
+
     if bird_rect.top <= -100 or bird_rect.bottom >= 900:
         return False
     return True
+
+
+def create_pipe():
+    random_pipe_pos = random.choice(pipe_height)
+    top_pipe = pipe_surface.get_rect(midbottom=(800, random_pipe_pos - 250))
+    bottom_pipe = pipe_surface.get_rect(midtop=(800, random_pipe_pos))
+    return bottom_pipe, top_pipe
+
+
+def move_pipes(pipes):
+    for pipe in pipes:
+        pipe.centerx -= 5
+
+    return pipes
+
+
+def draw_pipes(pipes):
+    for pipe in pipes:
+        if pipe.bottom >= 800:
+            screen.blit(pipe_surface, pipe)
+        else:
+            flip_pipe = pygame.transform.flip(pipe_surface, False, True)
+            screen.blit(flip_pipe, pipe)
+    return pipes
 
 
 # Variables
@@ -33,9 +61,21 @@ bird_rect = bird.get_rect(center=(100, 512))
 
 floor_base = pygame.image.load("assets/ground.png").convert()
 floor_base = pygame.transform.scale2x(floor_base)
-
-# Game Loop
 floor_x_pos = 0
+
+message = pygame.image.load("assets/Game_Over.png").convert_alpha()
+game_over_rect = message.get_rect(center=(288, 112))
+restart = pygame.image.load("assets/restart.png").convert_alpha()
+
+# Buliding pipes
+
+pipe_surface = pygame.image.load("assets/pipe.png")
+pipe_list = []
+pipe_height = [300, 400, 600]
+
+SPAWNPIPE = pygame.USEREVENT
+pygame.time.set_timer(SPAWNPIPE, 1200)
+
 game_active = True
 
 # start the game
@@ -50,21 +90,27 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE and game_active:
                 bird_movement = 0
-                bird_movement -= 12
+                bird_movement -= 10
             if event.key == pygame.K_SPACE and game_active == False:
                 bird_rect.centery = (100, 512)
                 bird_movement = 0
+                pipe_list.clear()
                 game_active = True
+        if event.type == SPAWNPIPE and game_active:
+            pipe_list.extend(create_pipe())
 
     screen.blit(background, (0, 0))
     if game_active:
         bird_movement += gravity
         bird_rect.centery += bird_movement
         screen.blit(bird, bird_rect)
+        # Draw pipes
+        pipe_list = move_pipes(pipe_list)
+        draw_pipes(pipe_list)
         # Check for collision
-        game_active = check_collision()
+        game_active = check_collision(pipe_list)
     else:
-        print("Game Over")
+        screen.blit(message, game_over_rect)
 
     # Create the floor
     floor_x_pos -= 1
